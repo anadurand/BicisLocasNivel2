4adc4813329093a0109e38c5b0b4a69c24668954
Ana Durand <ana.dc1004@gmail.com>
2017-04-04 14:02:42 -0500

    mejorando funciones

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 window.addEventListener("load",function(){
   var divBox = document.getElementsByClassName("input-box");
-  divBox[0].appendChild(createTooltip("Falta ingresar su nombre"));
-  divBox[1].appendChild(createTooltip("Falta ingresar su Apellido"));
-  divBox[2].appendChild(createTooltip("Falta ingresar su correo"));
-  divBox[3].appendChild(createTooltip("La contrseña debe tener al menos 6 caracteres"));
-  divBox[4].appendChild(createTooltip("Debe seleccionar al menos una Bici"));
+  //divBox[0].appendChild(createTooltip("Falta ingresar su nombre"));
+  //divBox[1].appendChild(createTooltip("Falta ingresar su Apellido"));
+  //divBox[2].appendChild(createTooltip("Falta ingresar su correo"));
+  //divBox[3].appendChild(createTooltip("La contrseña debe tener al menos 6 caracteres"));
+  //divBox[4].appendChild(createTooltip("Debe seleccionar al menos una Bici"));
 });
 
 function createTooltip(mensaje){
@@ -25,67 +25,94 @@ function validateForm(){
    var testName = /[A-Z]{1,}[a-z]+/;
    var testEmail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.com$/;
    var mens = "";
+    if(name.value == ""){
 
-   if(name.value == "" || !name.value.match(testName)){
-     mens = "La primera letra debe ser mayúscula";
-     change(name,mens);
-     name.focus();
- 	}else{
-    quit(name);
+      if(name.nextElementSibling == null){
+        divBox[0].appendChild(createTooltip("Falta ingresar su nombre"));
+      }
 
-    if(apell.value == "" || !apell.value.match(testName)){
-      mens = "La primera letra debe ser mayúscula";
-      change(apell,mens);
-      apell.focus();
     }else{
-      quit(apell);
+      if(!name.value.match(testName)){
+        mens = "La primera letra debe ser mayúscula";
+        change(name,mens);
+        name.focus();
+      }else{
+        quit(name);
 
-      if(mail.value == "" || !mail.value.match(testEmail)){
+      }
+    }
+
+    if(apell.value == ""){
+      if(apell.nextElementSibling == null){
+        divBox[1].appendChild(createTooltip("Falta ingresar su apellido"));
+      }
+
+    }else{
+      if(!apell.value.match(testName)){
+        mens = "La primera letra debe ser mayúscula";
+        change(apell,mens);
+        apell.focus();
+      }else{
+        quit(apell);
+
+      }
+    }
+    if(mail.value == ""){
+      if(mail.nextElementSibling == null){
+        divBox[2].appendChild(createTooltip("Falta ingresar su email"));
+      }
+
+    }else{
+      if(!mail.value.match(testEmail)){
         mens = "El Email debe ser válido Ejm. name@domain.com \n ";
         change(mail,mens);
         mail.focus();
       }else{
         quit(mail);
 
-        if(password.value == "" || password.value.length < 6 || password.value == '123456' || password.value == '098754' || password.value == 'password'){
-          mens = "El Password debe tener al menos 6 caracteres o diferente\n";
-          change(password,mens);
-          password.focus();
-        }else{
-          quit(password);
+      }
+    }
 
-          if(bici.value == 0){
-            mens = "Debe seleccionar una Bici\n";
-            change(bici,mens);
-            bici.focus();
-          }else{
-            removeTooltip();
-          }
-
-        }
-
+    if(password.value == ""){
+      if(password.nextElementSibling == null){
+        divBox[3].appendChild(createTooltip("Falta ingresar su password"));
       }
 
+    }else{
+      if((password.value.length < 6 || password.value == '123456') || (password.value == '098754' || password.value == 'password')){
+        mens = "El Password debe tener al menos 6 caracteres o diferente\n";
+        change(password,mens);
+        password.focus();
+      }else{
+        quit(password);
+
+      }
     }
-  }
 
+    if(bici.value == 0){
+      if(bici.nextElementSibling == null){
+        divBox[4].appendChild(createTooltip("Debe seleccionar al menos una Bici"));
+      }
+      bici.focus();
+    }else{
+      quit(bici);
 
+    }
 
 }
 
 //Remueve los tooltip
 function quit(nodo) {
-  if(nodo.nextSibling != null){
+  if(nodo.nextElementSibling != null){
     nodo.parentNode.removeChild(nodo.nextSibling);
   }
 }
-function removeTooltip(){
-  var array = document.getElementsByClassName("tool");
-  console.log(array);
-  for (i = 0; i < array.length; i++){
-    array[i].parentNode.removeChild(array[i]);
-  }
-}
+//cambia el texto del tooltip
 function change(nodo,text){
-  nodo.nextElementSibling.innerHTML = text;
+  if(nodo.nextElementSibling == null){
+    nodo.parentNode.appendChild(createTooltip(text));
+  }else{
+    nodo.nextElementSibling.textContent = text;
+
+  }
 }
