0aa57d8ae7196636e18a701ae413e69c61e33a83
anadurand <ana.dc1004@gmail.com>
2017-04-03 23:37:05 -0500

    tooltip

--- js/main.js
+++ js/main.js
@@ -1,36 +1,41 @@
 function validateForm(){
-	/* Escribe tú código aquí */
   var name = document.getElementById("name");
- var apell = document.getElementById("lastname").value;
- var password = document.getElementById("input-password").value;
- var mail = document.getElementById("input-email").value;
- var bici = document.getElementById("opciones").value;
+ var apell = document.getElementById("lastname");
+ var password = document.getElementById("input-password");
+ var mail = document.getElementById("input-email");
+ var bici = document.getElementById("opciones");
 
 //if((name != "" && apell  != "") && (password != "" && bici != "")){
 
   if(!name.value.match(/[A-Z]{1,}[a-z]+/)){
-    var span = document.createElement("span");
-    span.innerHTML = "Ingresar primera letra máyuscula";
-    name.parentNode.appendChild(span);
-    //alert("Error al ingresar Nombre: \n Ingresar primera letra máyuscula");
-  }
-  if(!apell.match(/[A-Z]{1,}[a-z]+/)){
-    alert("Error al ingresar Apellido: \n Ingresar primera letra máyuscula");
-  }
-  if(!mail.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.com$/)){
-    alert("Error al ingresar Email: \n Ingresar email@domain.com");
+    tooltip(name,"Ingresar primera letra máyuscula");
   }
 
-  if(password.length <= 6 || password == "password" || password == "123456" || password == "098754"){
-    alert("Valor de password invalido, \n Ingresar password de 6 caracteres");
+  if(!apell.value.match(/[A-Z]{1,}[a-z]+/)){
+    tooltip(apell,"Ingresar primera letra máyuscula");
   }
-  if(bici == "0"){
-    alert("Por favor Ingresar valor de bici");
+  if(!mail.value.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.com$/)){
+    tooltip(mail,"Error al ingresar Email: \n Ingresar email@domain.com");
+  }
+
+  if(password.value.length <= 6 || password == "password" || password == "123456" || password == "098754"){
+    tooltip(password,"Valor de password invalido, \n Ingresar password de 6 caracteres");
+  }
+  if(bici.value == "0"){
+    tooltip(bici,"Por favor Ingresar valor de bici");
   }
 
 
 //}else{
 //  alert("Falta Ingresar Valores");
 //}
+
+}
+
+function tooltip(nodo,texto){
+  var span = document.createElement("span");
+  span.innerHTML = texto;
+  nodo.parentNode.appendChild(span);
+
 
 }
